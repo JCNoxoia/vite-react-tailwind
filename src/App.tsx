3a7572e7e6f1081bee0c0e0c3ab1a94607ffd847
JCNoxoia <jcbarrett2000@protonmail.com>
2025-12-19 16:03:19 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex justify-center h-screen">
+      <div className="self-center text-center text-xl font-bold bg-teal-200 w-1/5 py-8 py-16 border-2 border-teal-500 rounded-lg">
+        Hello World!
+      </div>
+    </div>
+  )
+}
+
+export default App
